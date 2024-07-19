4b3f903a0be4e8583f4a042a8968a3aa111f512e
Timo Wickboldt <t.wickboldt@gmail.com>
2024-07-20 00:01:56 +0200

    replace zshrc settings in place instead of replacing .zshrc

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH
 #### set agnoster as theme, this came from https://gist.github.com/corentinbettiol/21a6d4e942a0ee58d51acb7996697a88
 #### in vscode settings for devcontainer (not for User or Workspace), Search for terminal.integrated.fontFamily value, and set it to "Roboto Mono for Powerline" (or any of those: https://github.com/powerline/fonts#font-families font families).
 
-cp .zshrc ~/.zshrc
 echo "export DEFAULT_USER=$(whoami)" >> ~/.zshrc
 echo "export EDITOR=vim" >> ~/.zshrc
+sed -i 's/ZSH_THEME.*$/ZSH_THEME="agnoster"/' ~/.zshrc
+sed -i 's/plugins=.*$/plugins=(git terraform zsh-autosuggestions zsh-syntax-highlighting terraform)/' ~/.zshrc
